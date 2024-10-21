9a61337cb98d464f5ae1b72ea57b1624f6c5e825
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-21 16:32:47 +0200

    Add task priority and completion functionality
    Introduced task priorities and completion markers in the TODO app. Modified forms and views to handle new priority and completion attributes. Added JSON support to persist tasks with added priorities and completion states.

--- app.rb
+++ app.rb
@@ -1,19 +1,48 @@
 require 'sinatra'
+require 'thin'
+require 'json'
 require 'sinatra/reloader' if development?
 
 TODO_ITEMS = []
+
+def load_tasks
+  if File.exist?('tasks.json') && !File.zero?('tasks.json')
+    file = File.read('tasks.json')
+    TODO_ITEMS.replace(JSON.parse(file))
+  end
+end
+
+def save_tasks
+  File.open('tasks.json', 'w') do |file|
+    file.write(TODO_ITEMS.to_json)
+  end
+end
+
+load_tasks
 
 get '/' do
   erb :index
 end
 
 post '/add' do
-  TODO_ITEMS << params[:task] unless params[:task].empty?
+  new_task = {
+    'task' => params[:task],
+    'completed' => false,
+    'priority' => params[:priority]
+  }
+  TODO_ITEMS << new_task unless params[:task].empty?
+  save_tasks
   redirect '/'
 end
 
 post '/delete' do
   TODO_ITEMS.delete_at(params[:index].to_i) if params[:index]
+  save_tasks
   redirect '/'
 end
 
+post '/complete' do
+  TODO_ITEMS[params[:index].to_i]['completed'] = true
+  save_tasks
+  redirect '/'
+end
